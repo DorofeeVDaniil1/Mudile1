c597dfb51689caae93ad9888c1319beac2085fcb
DorofeeVDaniil1 <82593323+DorofeeVDaniil1@users.noreply.github.com>
2021-06-14 09:13:31 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 int a2;
+	 double xx = .15;
+	 int x =5, y=6; //Можно записать в строчку
+        System.out.println(x+y);//ВЫвод 
+
+
+    }
+}
