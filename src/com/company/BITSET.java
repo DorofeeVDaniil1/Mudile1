dbdb48d6121ebb75462ef0b9f3b9216a79f939a9
DorofeeVDaniil1 <82593323+DorofeeVDaniil1@users.noreply.github.com>
2021-06-17 15:37:38 +0800

    Initial commit

---
+++ src/com/company/BITSET.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.BitSet;
+import java.util.Scanner;
+
+public class BITSET {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input value: ");
+        long value = scan.nextLong();
+        System.out.print("Input bit number: ");
+        int pos = scan.nextInt();
+
+        BitSet bs = BitSet.valueOf(new long[]{value});
+        bs.set(pos);
+        System.out.println(bs.toLongArray()[0]);
+    }
+
+
+}
+
---
+++ src/com/company/EXapmlt14.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class EXapmlt14 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        //a = (a >> b) << b;
+
+
+        //Операция xor
+//
+//        a=a^b;
+//        System.out.println(a+" "+b);
+//        b=a^b;
+//        System.out.println(a+" "+b);
+//        a=a^b;
+//        System.out.println(a+" "+b);
+
+        //<< Сдвиг в лево умножение на  2 в степени b - 2**b
+        a<<=b;
+        System.out.println("Сдвиг в лево << "+a+" "+b);
+
+        //Сдвиг в право >> деление на 2**b
+        a>>=b;
+        System.out.println("Сдвиг в право << "+a+" "+b);
+
+        //Фрагмент программы на Java с примерами быстрого умножения и деления на 2 с помощью операций поразрядного сдвига:
+
+        int i = 192;  //  00000000 00000000 00000000 11000000 (192)
+        i <<= 1;      //  00000000 00000000 00000001 10000000 (384)
+        i >>= 2;      //  00000000 00000000 00000000 01100000 (96)
+        int j =- 192; //  11111111 11111111 11111111 01000000 (-192)
+        j <<= 1;      //  11111111 11111111 11111110 10000000 (-384)
+        j >>= 2;      //  11111111 11111111 11111111 10100000 (-96)
+
+
+
+
+
+
+
+
+       // System.out.print(a);
+    }
+
+
+}
